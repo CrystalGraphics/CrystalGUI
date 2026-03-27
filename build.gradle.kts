8fb0664473d7c4885f9a9cc8a1eab6a9972a7e87
somehussar <148253652+somehussar@users.noreply.github.com>
2026-03-27 05:54:20 +0100

    Set-up Dev environment with Java 25 downgrading to 8

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+
+plugins {
+    id("com.gtnewhorizons.gtnhconvention")
+}
+
+// GTNH Gradle is forcing multirelease for 25,21 even if its set to empty in gradle.properties
+// I spent an hour trying to fix this shit - and if not that it's trying to release the original
+// no matter. this fixes it.
+// :p
+jvmdg.multiReleaseVersions.set(emptySet<JavaVersion>())
+jvmdg.multiReleaseOriginal.set(false)
